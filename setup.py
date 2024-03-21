56d881323453e80bfdaa323a77891f714016fa67
ayuk977 <ayuk977@gmail.com>
2024-03-21 19:18:48 +0700

    feat: Add init file

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="alertg",
+    version="1.0.0",
+    license="MIT",
+    url="https://github.com/ayuk977/alertg",
+    author="Zackzonexx",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author_email="ayuk977@gmail.com",
+    packages=find_packages(),
+    keywords="alertg",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",  # Specify your license type
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Natural Language :: English",
+    ],
+    install_requires=[
+        "opsgenie-sdk",
+        "requests",
+        "urllib3",
+        "dmacheck",
+    ],
+    entry_points={
+        "console_scripts": [
+            "alertg=app:main",
+        ],
+    },
+    zip_safe=False,
+)
